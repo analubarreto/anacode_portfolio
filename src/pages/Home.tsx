5b0fa724fcfd82439b0420c2f0fa24679e7860e0
analubarreto <analuibm@gmail.com>
2024-04-11 16:42:44 -0300

    Started home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Main = styled.section``;
+
+
+const Home = (): JSX.Element => {
+  return (
+    <Main>
+      <p>Home</p>
+    </Main>
+  )
+}
+
+export default Home
