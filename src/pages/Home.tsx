af0bffdd6acadb6e54963d26fa8989ef04651e90
analubarreto <analuibm@gmail.com>
2024-04-11 16:57:30 -0300

    Added i18n support for the plugin.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
 const Main = styled.section``;
 
-
 const Home = (): JSX.Element => {
+  const { t } = useTranslation();
+
   return (
     <Main>
-      <p>Home</p>
+      <h1>Ana Code</h1>
+      <h3>{ t('Unblocking your digital potential') }</h3>
     </Main>
   )
 }
