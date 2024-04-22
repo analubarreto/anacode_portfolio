08712fca370e99b9e79a1059f5ac209fa76f790d
analubarreto <analuibm@gmail.com>
2024-04-22 16:26:46 -0300

    Added clickoutside in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import Navigation from '@/components/Header/Navigation';
 import LanguageToggle from '@/components/Header/LanguageToggle';
 import { t } from 'i18next';
 import { HeaderMain, Title } from '@/components/Header/styles/index.styles';
+import { useRef, useEffect } from 'react';
 
 type HeaderProps = {
   closeMenu: () => void;
@@ -12,18 +13,33 @@ type HeaderProps = {
 }
 
 /**
- * Renders the header component.
+ * Header component is closed when user clicks outside HeaderMain.
  *
- * @param {Object} props - The component props.
- * @param {Function} props.themeToggle - The function to toggle the theme.
- * @param {boolean} props.showMenu - A boolean indicating whether the menu is shown.
- * @param {string} props.themeName - The name of the current theme.
- * @param {Function} props.closeMenu - The function to close the menu.
- * @returns {JSX.Element} The rendered header component.
+ * @param themeToggle - Function to toggle the theme.
+ * @param showMenu - Boolean indicating whether the menu is shown or not.
+ * @param themeName - Current theme name.
+ * @param closeMenu - Function to close the menu.
+ * @returns The rendered Header component.
  */
 const Header = ({ themeToggle, showMenu, themeName, closeMenu }: HeaderProps): JSX.Element => {  
+  const headerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (headerRef.current && !headerRef.current.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closeMenu]);
+
   return (
-    <HeaderMain $showMenu={showMenu}>
+    <HeaderMain $showMenu={showMenu} ref={headerRef}>
       <Title>
         <button className='color-mode-toggle' onClick={themeToggle}>
           { themeName === 'light' ? <Moon size={30} /> : <Sun size={30} /> }
