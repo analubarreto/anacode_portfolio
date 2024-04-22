6933c7340726d22c041ba3ea6bf0691cbc328cbb
analubarreto <analuibm@gmail.com>
2024-04-22 12:39:14 -0300

    Refactor in app component

---
+++ src/contexts/HomeSectionsContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useRef, useContext } from 'react';
+
+type HomeSectionsContextType = React.MutableRefObject<HTMLDivElement | null>;
+type HomeSectionsProviderProps = {
+  children: React.ReactNode;
+}
+
+// Define the context
+const HomeSectionsContext = createContext<HomeSectionsContextType | null>(null);
+
+// Define a provider component
+const HomeSectionsProvider = ({ children }: HomeSectionsProviderProps) => {
+  const mainRef = useRef<HTMLDivElement | null>(null);
+  
+  return (
+    <HomeSectionsContext.Provider value={mainRef}>
+      {children}
+    </HomeSectionsContext.Provider>
+  );
+};
+
+// Define a custom hook for accessing the context
+const useMainRef = () => {
+  const context = useContext(HomeSectionsContext);
+  if (context === undefined) {
+    throw new Error('useMainRef must be used within a HomeSectionsProvider');
+  }
+  return context;
+};
+
+export { HomeSectionsProvider, useMainRef };
