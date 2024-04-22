28195828de0a779a419cba916d33ab411a191ef0
analubarreto <analuibm@gmail.com>
2024-04-22 11:54:08 -0300

    Removed ts-ignore comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,38 +18,30 @@ const Main = styled.main<{ theme: any }>`
 
 const Home = (): JSX.Element => {
   const [, setActiveLink] = useActiveLink();
-  const mainRef = useRef(null);
+  const mainRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     console.log('useEffect')
 
     const handleScroll = () => {
       if (!mainRef.current) return;
-      if (mainRef.current) {
-        const sections = document.getElementsByClassName('section');
-        const currentSection = Array.from(sections).find(section => {
-          const rect = section.getBoundingClientRect();
-          return rect.top <= 0 && rect.bottom > 0;
-        });
+      const sections = document.getElementsByClassName('section');
+      const currentSection = Array.from(sections).find(section => {
+        const rect = section.getBoundingClientRect();
+        return rect.top <= 0 && rect.bottom > 0;
+      });
 
-        if (currentSection) {
-          const id = currentSection.getAttribute('id');
-          const link = links.find(link => link.elementId === id) || links[0];
-          setActiveLink(link.id);
-        }
+      if (currentSection) {
+        const id = currentSection.getAttribute('id');
+        const link = links.find(link => link.elementId === id) || links[0];
+        setActiveLink(link.id);
       }
     };
 
-    if (mainRef.current) {
-      // @ts-ignore
-      mainRef.current.addEventListener('scroll', handleScroll);
-    }
+    mainRef.current?.addEventListener('scroll', handleScroll);
 
     return () => {
-      if (mainRef.current) {
-        // @ts-ignore
-        mainRef.current.removeEventListener('scroll', handleScroll);
-      }
+      mainRef.current?.removeEventListener('scroll', handleScroll);
     }
   }, []);
 
