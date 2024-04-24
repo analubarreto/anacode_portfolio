d408f53a00ac801e0aee63c6e1351206c23f9a08
analubarreto <analuibm@gmail.com>
2024-04-24 14:47:08 -0300

    Updated for mobile devices

--- src/components/Sections/styles/AboutUs.styles.tsx
+++ src/components/Sections/styles/AboutUs.styles.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const AboutUsSection = styled.section<{ theme: any }>`
   &.main-about-us {
-    min-height: 100vh;
     width: 100vw;
     background-color: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
@@ -24,7 +23,7 @@ export const AboutUsSection = styled.section<{ theme: any }>`
 
     p {
       font-size: 1.6rem;
-      width: 37rem;
+      width: 34rem;
 
       @media (min-width: 1024px) {
         font-size: 2rem;
@@ -44,39 +43,71 @@ export const AboutUsSection = styled.section<{ theme: any }>`
       }
 
       &__title {
-        font-size: 2.4rem;
-        margin-bottom: 2rem;
+        font-size: 1.6rem;
+        margin-bottom: 4rem;
         text-transform: uppercase;
-        margin-bottom: 7rem;
+
+        @media screen and (min-width: 1024px) {
+          font-size: 2.4rem;
+          margin-bottom: 7rem;
+        }
       }
 
       &__members {
         display: flex;
         width: 100%;
+        flex-wrap: wrap;
+        flex-direction: column;
+
+        @media screen and (min-width: 1024px) {
+          flex-direction: row;
+          flex-wrap: nowrap;
+        }
 
         h4 {
-          font-size: 2rem;
-          margin-bottom: 3rem;
+          font-size: 1.6rem;
+          margin-bottom: 1rem;
+
+          @media screen and (min-width: 1024px) {
+            font-size: 2rem;
+            margin-bottom: 3rem;
+          }
         }
 
         p {
-          font-size: 2rem;
-          width: 44rem;
+          font-size: 1.6rem;
+          width: 32rem;
+
+          @media screen and (min-width: 1024px) {
+            font-size: 2rem;
+            width: 44rem;
+          }
 
           span {
             color: ${({ theme }) => theme.primary};
             cursor: pointer;
-            margin-left: 50%;
+            margin-left: 1rem;
+
+            @media screen and (min-width: 1024px) {
+              margin-left: 50%;
+            }
           }
         }
         
         &--member {
           display: flex;
-          align-items: flex-start;
+          align-items: center;
+          flex-direction: column;
           gap: 1rem;
           padding: 1rem;
           border-radius: 20px;
-          background-color: ${({ theme }) => theme.body};
+          margin-top: 3rem;
+
+          @media screen and (min-width: 1024px) {
+            align-items: flex-start;
+            flex-direction: row;
+            margin-top: 0%;
+          }
 
           .text-wrapper {
             display: flex;
@@ -85,6 +116,11 @@ export const AboutUsSection = styled.section<{ theme: any }>`
             .linkedin {
               margin-top: 2rem;
               color: ${({ theme }) => theme.linkedIn};
+
+              @media screen and (min-width: 1024px) {
+                margin-top: 2rem;
+                color: ${({ theme }) => theme.linkedIn};
+              }
             }
 
             p > span {
@@ -94,22 +130,33 @@ export const AboutUsSection = styled.section<{ theme: any }>`
 
           .image-wrapper {
             position: relative;
-            width: 35rem;
-            height: 32rem;
+            width: 20.5rem;
+            height: 25.6rem;
             overflow: visible;
             border-radius: 20rem;
             background-color: ${({ theme }) => theme.imageBackground};
             margin-right: 2rem;
 
+            @media screen and (min-width: 1024px) {
+              width: 35rem;
+              height: 32rem;
+            }
+
             img {
-              width: 40rem;
-              height: 40rem;
+              width: 30rem;
+              height: 30rem;
               border-radius: 20rem;
               object-fit: cover;
               position: absolute;
               top: 50%;
               left: 50%;
-              transform: translate(-50%, -60%);
+              transform: translate(-50%, -57.5%);
+
+              @media screen and (min-width: 1024px) {
+                width: 40rem;
+                height: 40rem;
+                transform: translate(-50%, -60%);
+              }
             }
           }
         }
