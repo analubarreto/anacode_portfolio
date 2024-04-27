38b0b4ff3c5bc021ff8783b4b7e916e241c45e09
analubarreto <analuibm@gmail.com>
2024-04-27 12:13:17 -0300

    Changes to Welcome

--- src/components/Sections/Welcome.tsx
+++ src/components/Sections/Welcome.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from 'react-i18next';
-import Button from '@/components/Button';
 import { Main, Section, Title } from '@/components/Sections/styles/Welcome.styles';
 
 type WelcomeProps = {
@@ -29,10 +28,6 @@ const Welcome = ({ id }: WelcomeProps): JSX.Element => {
           </div>
           <h3 data-testid='subtitle'>{ t('Unblocking your digital potential').toUpperCase() }</h3>
         </Title>
-      </Section>
-      <Section>
-        <p>{ t('Presentation') }</p>
-        <Button href='#contact'>{ t('Contact us') }</Button>
       </Section>
     </Main>
   )
