93ed6b19e6a35b172227fd081746963dd2aaf10f
analubarreto <analuibm@gmail.com>
2024-05-01 09:33:14 -0300

    Contact page

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,16 +1,14 @@
 //@ts-ignore
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
-// import { useTheme } from '@/hooks/useTheme';
-import { Section } from '@/components/Sections/styles/Contact.styles';
+import { useTranslation } from 'react-i18next';
+import { Section, CompanyInfo } from '@/components/Sections/styles/Contact.styles';
 
 type ProjectsProps = {
   id: string;
 };
 
 const Contact = ({ id }: ProjectsProps): JSX.Element => {
-  // const { t } = useTranslation();
-  // const { theme } = useTheme();
+  const { t } = useTranslation();
 
   return (
     <Section
@@ -20,7 +18,30 @@ const Contact = ({ id }: ProjectsProps): JSX.Element => {
       whileInView={{ scale: 1, y: 0 }}
       viewport={{ once: false }}
     >
-      <p>Contact</p>
+      <h1>{ t('Contact Us') }</h1>
+
+      <section>
+
+        <CompanyInfo>
+          <div>
+            <h3>{ t('Working Hours') }</h3>
+            <p>{ t('Monday to Friday') }</p>
+            <p>09:00 - 18:00</p>
+            <p>{ t('Sat, Sun, Holidays') }</p>
+            <p>{ t('Closed') }</p>
+          </div>
+
+          <div>
+            <h3>{ t('Support') }</h3>
+            <a href="mailto:dromcoders@gmail.com" target="_blank">dromcoders@gmail.com</a>
+            <span>
+              <a href="https://w.app/anadromcode" target="_blank">WhatsApp Ana</a>
+              <a href="https://w.app/rogerdromcode" target='_blank'>WhatsApp Roger</a>
+              <a href="https://www.instagram.com/drom_code/" target='_blank' rel='noopener noreferrer'>@drom_code</a>
+            </span>
+          </div>
+        </CompanyInfo>
+      </section>
     </Section>
   )
 }
--- src/components/Sections/styles/Contact.styles.tsx
+++ src/components/Sections/styles/Contact.styles.tsx
@@ -5,11 +5,74 @@ export const Section = styled(motion.section)<{ theme: any }>`
   &.main-contact {
     display: flex;
     flex-direction: column;
-    align-items: center;
     justify-content: center;
-    min-height: 100vh;
+    align-items: center;
     width: 100vw;
     background-color: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
+    padding: 8rem 2rem 5rem 2rem;
+
+    @media screen and (min-width: 1024px) {
+      padding: 12rem 10rem 15rem 10rem;
+      height: 100vh;
+    }
+
+    h1 {
+      font-size: 2.4rem;
+      margin-bottom: 2rem;
+      align-self: flex-start;
+
+      @media screen and (min-width: 1024px) {
+        font-size: 4rem;
+      }
+    }
+
+    section {
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+
+      @media screen and (min-width: 1024px) {
+        flex-direction: row;
+      }
+      
+      h3 {
+        font-size: 1.6rem;
+        margin-bottom: 1rem;
+
+        @media screen and (min-width: 1024px) {
+          font-size: 2rem;
+          margin-bottom: 2rem;
+        }
+      }
+
+      p, a {
+        display: block;
+        font-size: 1.4rem;
+        margin-bottom: 1rem;
+
+        @media screen and (min-width: 1024px) {
+          font-size: 1.6rem;
+        }
+      }
+    }
+  }
+`;
+
+export const CompanyInfo = styled.article`
+  display: flex;
+  flex-direction: column;
+  height: 30rem;
+  justify-content: space-between;
+  background-color: #DCA1B0;
+  padding: 2rem;
+  border-radius: 2rem;
+
+  @media screen and (min-width: 1024px) {
+    height: 100%;
+    width: 50rem;
+    height: 40rem;
+    border-radius: 2rem;
+    margin-top: 5rem;
   }
 `;
