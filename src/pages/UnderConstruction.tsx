5f0d1b7cd667d4041229ab2027b47979913bceb0
analubarreto <analuibm@gmail.com>
2024-05-02 13:56:13 -0300

    Documented under constrction page

--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -2,6 +2,10 @@ import Button from '@/components/Button';
 import { MainDiv } from '@/pages/styles/UnderConstruction.styles';
 import { useTranslation } from 'react-i18next';
 
+/**
+ * Renders the under construction page.
+ * @returns The JSX element representing the under construction page.
+ */
 const UnderConstruction = (): JSX.Element => {
   const { t } = useTranslation();
 
