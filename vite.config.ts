5502487e94e5b92668a445b3d00617ca97b5d857
analubarreto <analuibm@gmail.com>
2024-05-06 11:58:04 -0300

    Added .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  const cwd = process.cwd()
+  const env = loadEnv(mode, cwd)
+
+  return {
+    define: {
+      'process.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
+      'process.env.VITE_AUTH_DOMAIN': JSON.stringify(env.VITE_AUTH_DOMAIN),
+      'process.env.VITE_PROJECT_ID': JSON.stringify(env.VITE_PROJECT_ID),
+      'process.env.VITE_STORAGE_BUCKET': JSON.stringify(env.VITE_STORAGE_BUCKET),
+      'process.env.VITE_MESSAGING_SENDER_ID': JSON.stringify(env.VITE_MESSAGING_SENDER_ID),
+      'process.env.VITE_APP_ID': JSON.stringify(env.VITE_APP_ID),
+      'process.env.VITE_MEASUREMENT_ID': JSON.stringify(env.VITE_MEASUREMENT_ID),
+    },
+    server: {
+      port: 3000,
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
     }
-  }
-})
+  };
+});
